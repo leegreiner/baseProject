8f11fd03e160ae0d8676c8bd9c0f48a5ae4c5747
leegreiner <lmg@yahoo.com>
2019-03-08 18:18:33 -0500

    Initial commit.

---
+++ src/main/java/edu/duke/rs/baseProject/BaseProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.duke.rs.baseProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/duke/rs/baseProject/BaseProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.duke.rs.baseProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
