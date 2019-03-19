256d2aed67bb1ce63886106200eb334ec0702bb0
leegreiner <lmg@yahoo.com>
2019-03-19 17:36:24 -0400

    Added logging.

--- src/main/java/edu/duke/rs/baseProject/index/IndexController.java
+++ src/main/java/edu/duke/rs/baseProject/index/IndexController.java
@@ -3,10 +3,14 @@ package edu.duke.rs.baseProject.index;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class IndexController {
 	@GetMapping("/")
 	public String index() {
+		log.debug("In index()");
 		return "index";
 	}
 }
