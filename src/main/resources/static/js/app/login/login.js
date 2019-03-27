faba275d350c54c7dd6cf9fc5ac55b1d2f772894
leegreiner <lmg@yahoo.com>
2019-03-27 13:56:07 -0400

    Made login page reactive and removed login button.

---
+++ src/main/resources/static/js/app/login/login.js
@@ -0,0 +1,34 @@
+var $loginForm = $("#login-form").validate({
+	errorClass		: 'invalid',
+	errorElement	: 'em',
+	highlight: function(element) {
+	    $(element).parent().removeClass('state-success').addClass("state-error");
+	    $(element).removeClass('valid');
+	},
+	unhighlight: function(element) {
+	    $(element).parent().removeClass("state-error").addClass('state-success');
+	    $(element).addClass('valid');
+	},
+	
+	rules : {
+		username : {
+			required : true
+		},
+		password : {
+			required : true
+		}
+	},
+	
+	messages : {
+		username : {
+			required : 'Please enter your user name'
+		},
+		password : {
+			required : 'Please enter your password'
+		}
+	},
+
+	errorPlacement : function(error, element) {
+		error.insertAfter(element.parent());
+	}
+});
