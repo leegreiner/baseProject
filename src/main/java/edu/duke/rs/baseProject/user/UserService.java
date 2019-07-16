27c41f878baa25d07d882b32c175222dd13d37a9
grein003 <grein003@DEC-PF1P80J7.dhe.duke.edu>
2019-07-16 13:37:07 -0400

    Added current user name to page header.

--- src/main/java/edu/duke/rs/baseProject/user/UserService.java
+++ src/main/java/edu/duke/rs/baseProject/user/UserService.java
@@ -1,9 +1,5 @@
 package edu.duke.rs.baseProject.user;
 
-import javax.validation.constraints.NotNull;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
