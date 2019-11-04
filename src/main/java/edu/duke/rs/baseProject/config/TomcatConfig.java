d15bfd5f374af05f7e31d5bd53d0177c5eef13c4
grein003 <grein003@DEC-PF1P80J7.mshome.net>
2019-11-04 11:38:17 -0500

    Added Tomcat redirect to servrer.port. Added menu on top toggle.

--- src/main/java/edu/duke/rs/baseProject/config/TomcatConfig.java
+++ src/main/java/edu/duke/rs/baseProject/config/TomcatConfig.java
@@ -1,14 +1,22 @@
 package edu.duke.rs.baseProject.config;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TomcatConfig {
+  @Value("${server.port}")
+  private int serverPort;
+  
+  /*
   @Bean
   public ConfigurableServletWebServerFactory webServerFactory() {
     final TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
@@ -16,11 +24,41 @@ public class TomcatConfig {
     
     return factory;
   }
+  */
   
-  private static class CustomTomcatConnectorCustomizer implements TomcatConnectorCustomizer {
+  @Bean
+  public ServletWebServerFactory servletContainer() {
+      TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+          @Override
+          protected void postProcessContext(Context context) {
+              SecurityConstraint securityConstraint = new SecurityConstraint();
+              securityConstraint.setUserConstraint("CONFIDENTIAL");
+              SecurityCollection collection = new SecurityCollection();
+              collection.addPattern("/*");
+              securityConstraint.addCollection(collection);
+              context.addConstraint(securityConstraint);
+          }
+      };
+      tomcat.addConnectorCustomizers(new RelaxedQueryTomcatCustomizer());
+      tomcat.addAdditionalTomcatConnectors(redirectConnector());
+      return tomcat;
+  }
+
+  
+  private static class RelaxedQueryTomcatCustomizer implements TomcatConnectorCustomizer {
     @Override
-    public void customize(Connector connector) {
+    public void customize(final Connector connector) {
         connector.setProperty("relaxedQueryChars", "|{}[]");
     }
   }
+  
+  private Connector redirectConnector() {
+    Connector connector
+            = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    connector.setScheme("http");
+    connector.setSecure(false);
+    connector.setPort(8080);
+    connector.setRedirectPort(serverPort);
+    return connector;
+ }
 }
