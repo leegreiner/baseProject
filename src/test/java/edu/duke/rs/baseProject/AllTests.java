1f75078a2d5483e482dee126ab78a5ce6dcb8dcf
grein003 <grein003@host.docker.internal>
2020-02-27 08:00:30 -0500

    Added unit, integration, and all test suites.

---
+++ src/test/java/edu/duke/rs/baseProject/AllTests.java
@@ -0,0 +1,12 @@
+package edu.duke.rs.baseProject;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("edu.duke.rs.baseProject")
+@IncludeClassNamePatterns({"^.*Test?$"})
+public class AllTests {
+}
