239d796cafd3a3597688cb88b1ddebbd9837af4e
grein003 <grein003@host.docker.internal>
2020-03-10 12:48:38 -0400

    Updated alterschema.sql for change in user name.

--- src/db/alterSchema.sql
+++ src/db/alterSchema.sql
@@ -1,3 +1,3 @@
-CREATE UNIQUE INDEX UIX_USER_NAME ON users (upper(user_name));
+CREATE UNIQUE INDEX UIX_USER_NAME ON users (upper(username));
 CREATE UNIQUE INDEX UIX_EMAIL ON users (upper(email));
 CREATE UNIQUE INDEX UIX_ROLE_NAME ON role (upper(name));
