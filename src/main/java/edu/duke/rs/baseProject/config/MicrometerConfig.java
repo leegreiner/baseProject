0afc480b0747eac8731f63671f57071dcfb3d41c
grein003 <grein003@host.docker.internal>
2020-03-27 18:17:15 -0400

    Upgraded to Boot 2.2.5.

--- src/main/java/edu/duke/rs/baseProject/config/MicrometerConfig.java
+++ src/main/java/edu/duke/rs/baseProject/config/MicrometerConfig.java
@@ -1,5 +1,7 @@
 package edu.duke.rs.baseProject.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,14 @@ import io.micrometer.core.instrument.MeterRegistry;
 
 @Configuration
 public class MicrometerConfig {
+  @Value("${spring.application.name}")
+  private String applicationName;
+  
+  @Bean
+  MeterRegistryCustomizer<MeterRegistry> initializeRegistry() {
+    return registry -> registry.config().commonTags("application", applicationName);
+  }
+  
   @Bean
   public TimedAspect timedAspect(final MeterRegistry registry) {
       return new TimedAspect(registry);
