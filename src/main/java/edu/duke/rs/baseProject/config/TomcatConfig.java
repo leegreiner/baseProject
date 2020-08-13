7668b9ee2f6142eab40bd1f810d2549e7e2e3c03
grein003 <grein003@DEC-PF1P80J7.dhe.duke.edu>
2020-08-13 15:49:23 -0400

    Added session timeout to HazelcastConfig. Added graceful shutdown.

--- src/main/java/edu/duke/rs/baseProject/config/TomcatConfig.java
+++ src/main/java/edu/duke/rs/baseProject/config/TomcatConfig.java
@@ -1,22 +1,37 @@
 package edu.duke.rs.baseProject.config;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.threads.ThreadPoolExecutor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextClosedEvent;
 
+import com.hazelcast.core.HazelcastInstance;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
   @Value("${server.port:8080}")
   private int serverPort;
   @Value("${server.ssl.enabled:false}")
   private boolean sslEnabled;
+  private final HazelcastInstance hazelcastInstance;
 
   @Override
-  public void customize(TomcatServletWebServerFactory factory) {
+  public void customize(final TomcatServletWebServerFactory factory) {
     factory.addConnectorCustomizers(new RelaxedQueryTomcatCustomizer());
+    factory.addConnectorCustomizers(new GracefulShutdown(hazelcastInstance));
 
     if (sslEnabled) {
       factory.addAdditionalTomcatConnectors(redirectConnector());
@@ -37,5 +52,41 @@ public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWeb
     connector.setPort(8080);
     connector.setRedirectPort(serverPort);
     return connector;
+  }
+  
+  private static final class GracefulShutdown implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+    private volatile Connector connector;
+    private final HazelcastInstance hazelcastInstance;
+    
+    public GracefulShutdown(final HazelcastInstance hazelcastInstance) {
+      this.hazelcastInstance = hazelcastInstance;
+    }
+    
+    @Override
+    public void onApplicationEvent(final ContextClosedEvent event) {
+      this.connector.pause();
+      
+      final Executor executor = this.connector.getProtocolHandler().getExecutor();
+      
+      if (executor instanceof ThreadPoolExecutor) {
+          try {
+              ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
+              threadPoolExecutor.shutdown();
+              if (!threadPoolExecutor.awaitTermination(60, TimeUnit.SECONDS)) {
+                  log.warn("Tomcat thread pool did not shut down gracefully within "
+                          + "60 seconds. Proceeding with forceful shutdown");
+              }
+          } catch (InterruptedException ex) {
+              Thread.currentThread().interrupt();
+          }
+      }
+      
+      hazelcastInstance.shutdown();
+    }
+
+    @Override
+    public void customize(final Connector connector) {
+      this.connector = connector;
+    }
   }
 }
