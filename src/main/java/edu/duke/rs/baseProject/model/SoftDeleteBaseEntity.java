27563cd15625df2cb311080f4967c7e9a814ca68
grein003 <grein003@host.docker.internal>
2021-01-29 12:03:32 -0500

    Added ability to soft delete entities.

---
+++ src/main/java/edu/duke/rs/baseProject/model/SoftDeleteBaseEntity.java
@@ -0,0 +1,40 @@
+package edu.duke.rs.baseProject.model;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PreRemove;
+
+import org.hibernate.annotations.Type;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@MappedSuperclass
+//All entities must have the following 2 annotations with tableName and idName replaced
+//  @SQLDelete(sql = "UPDATE tableName SET deleted = 'Y' WHERE idName = ? AND version = ?", check = ResultCheckStyle.COUNT)
+//  @Where(clause = "deleted is not null")
+//For associations you may need the following depending on if a join table is involved
+//  @Where(clause = "deleted is null")
+//  @WhereJoinTable(clause = "deleted is null")
+//  @SQLDeleteAll(sql=)
+//If a join table is involved you must create a manual join table that extends from this class and
+// the above annotations for associations apply.
+//You may also want to index the deleted field if many entities exist
+public class SoftDeleteBaseEntity extends BaseEntity {
+  @Column(name = "deleted", nullable = true)
+  @Type(type = "yes_no")
+  private Boolean deleted;
+  
+  public void undelete() {
+    deleted = null;
+  }
+  
+  @PreRemove
+  public void remove() {
+    deleted = Boolean.TRUE;
+  }
+}
