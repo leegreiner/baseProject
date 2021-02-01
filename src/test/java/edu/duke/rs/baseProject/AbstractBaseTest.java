640a9fc32659f2e26f6529749efb4c8c98736935
grein003 <grein003@DEC-PF1P80J7.dhe.duke.edu>
2021-02-01 15:10:45 -0500

    Run junit tests in UTC.

--- src/test/java/edu/duke/rs/baseProject/AbstractBaseTest.java
+++ src/test/java/edu/duke/rs/baseProject/AbstractBaseTest.java
@@ -1,5 +1,8 @@
 package edu.duke.rs.baseProject;
 
+import java.util.TimeZone;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 import org.springframework.test.context.ActiveProfiles;
@@ -7,4 +10,9 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 public abstract class AbstractBaseTest {
   protected static final ProjectionFactory PROJECTION_FACTORY = new SpelAwareProxyProjectionFactory();
+  
+  @BeforeAll
+  public static void beforeAllTest() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
 }
