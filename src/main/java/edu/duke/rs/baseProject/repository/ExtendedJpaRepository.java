1a028e9759c2e16cb8c5d56da6145b4faf3de9f2
grein003 <grein003@host.docker.internal>
2021-02-15 11:24:58 -0500

    Have custom repository extend from JpaSpecificationExecutor.

--- src/main/java/edu/duke/rs/baseProject/repository/ExtendedJpaRepository.java
+++ src/main/java/edu/duke/rs/baseProject/repository/ExtendedJpaRepository.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import edu.duke.rs.baseProject.model.BaseEntity;
 
 @NoRepositoryBean
-public interface ExtendedJpaRepository<T extends BaseEntity, ID extends Serializable> extends JpaRepository<T, ID> {
+public interface ExtendedJpaRepository<T extends BaseEntity, ID extends Serializable>
+  extends JpaRepository<T, ID>,  JpaSpecificationExecutor<T> {
   Optional<T> findByAlternateId(UUID alternateId);
   Optional<T> findByAlternateId(UUID alternateId, String entityGraphName);
 }
