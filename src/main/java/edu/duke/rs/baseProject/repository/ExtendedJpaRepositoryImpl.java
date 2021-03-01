08b819355e14f09a880aab148e0bbc5c2a0fee39
grein003 <grein003@DEC-PF1P80J7.dhe.duke.edu>
2021-03-01 09:51:30 -0500

    Use modelgen classes in ExtendedJpaRepositoryImpl.

--- src/main/java/edu/duke/rs/baseProject/repository/ExtendedJpaRepositoryImpl.java
+++ src/main/java/edu/duke/rs/baseProject/repository/ExtendedJpaRepositoryImpl.java
@@ -11,14 +11,15 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.hibernate.jpa.QueryHints;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import edu.duke.rs.baseProject.model.BaseEntity;
+import edu.duke.rs.baseProject.model.BaseEntity_;
 
 public class ExtendedJpaRepositoryImpl<T extends BaseEntity, ID extends Serializable> extends SimpleJpaRepository<T, ID>
     implements ExtendedJpaRepository<T, ID> {
-  private static final String ALTERNATE_ID = "alternateId";
   private final EntityManager entityManager;
   
   public ExtendedJpaRepositoryImpl(final JpaEntityInformation<T, ?> entityInformation, final EntityManager entityManager) {
@@ -42,12 +43,12 @@ public class ExtendedJpaRepositoryImpl<T extends BaseEntity, ID extends Serializ
     final Root<T> root = query.from(this.getDomainClass());
     
     query.select(root)
-      .where(builder.equal(root.get(ALTERNATE_ID), alternateId));
+      .where(builder.equal(root.get(BaseEntity_.alternateId), alternateId));
     
     final TypedQuery<T> typedQuery = entityManager.createQuery(query);
     
     if (entityGraphName != null && ! entityGraphName.isEmpty()) {
-      typedQuery.setHint("javax.persistence.loadgraph", entityManager.getEntityGraph(entityGraphName));
+      typedQuery.setHint(QueryHints.HINT_LOADGRAPH, entityManager.getEntityGraph(entityGraphName));
     }
     
     try {
